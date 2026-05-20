5c6d763a6e66fb6e97e0d464d385a6f7a2b5e041
Manishraii-web <raimanish489@gmail.com>
2026-05-20 15:47:43 +0545

    updates

--- app/Http/Requests/Auth/RegisterRequest.php
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -25,9 +25,7 @@ class RegisterRequest extends FormRequest
        return [
 
     'name' => 'required|string|max:255',
-
     'email' => 'required|email|unique:admins,email',
-
     'password' => 'required|min:6|confirmed',
 
 ];
