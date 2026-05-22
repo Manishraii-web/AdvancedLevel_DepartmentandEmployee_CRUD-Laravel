3db8abd6b0b3af3662c32897a4302cb23408dc7c
Manishraii-web <raimanish489@gmail.com>
2026-05-22 12:54:55 +0545

    errors solving

--- routes/employee-auth.php
+++ routes/employee-auth.php
@@ -3,40 +3,34 @@
 use App\Http\Controllers\Employee\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest:employee')->group(function () {
+Route::get(
+    '/employee/login',
+    [AuthController::class,'showLogin']
+)->name('employee.login');
 
-    Route::get(
-        '/employee/login',
-        [AuthController::class,'showLogin']
-    )->name('employee.login');
+Route::post(
+    '/employee/login',
+    [AuthController::class,'login']
+)->name('employee.login.submit');
 
-    Route::post(
-        '/employee/login',
-        [AuthController::class,'login']
-    )->name('employee.login.submit');
+Route::get(
+    '/employee/register',
+    [AuthController::class,'showRegister']
+)->name('employee.register');
 
-    Route::get(
-        '/employee/register',
-        [AuthController::class,'showRegister']
-    )->name('employee.register');
+Route::post(
+    '/employee/register',
+    [AuthController::class,'register']
+)->name('employee.register.submit');
 
-    Route::post(
-        '/employee/register',
-        [AuthController::class,'register']
-    )->name('employee.register.submit');
-});
+Route::post(
+    '/employee/logout',
+    [AuthController::class,'logout']
+)->name('employee.logout');
 
-Route::middleware('auth:employee')->group(function () {
-
-    Route::post(
-        '/employee/logout',
-        [AuthController::class,'logout']
-    )->name('employee.logout');
-
-    Route::get(
-        '/employee/dashboard',
-        function () {
-            return 'Employee Dashboard';
-        }
-    )->name('employee.dashboard');
-});
+Route::get(
+    'employee/dashboard',
+    function () {
+        return 'Employee Dashboard';
+    }
+)->name('employee.dashboard');
