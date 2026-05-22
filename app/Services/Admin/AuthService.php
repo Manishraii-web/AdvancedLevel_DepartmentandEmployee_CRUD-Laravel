b11a156335299e3375267017bad613ebc164d0be
Manishraii-web <raimanish489@gmail.com>
2026-05-22 13:03:01 +0545

    finally all bugs fixed

--- app/Services/Admin/AuthService.php
+++ app/Services/Admin/AuthService.php
@@ -1,46 +1,46 @@
 <?php
 
 namespace App\Services\Admin;
+
 use Illuminate\Support\Facades\Auth;
 use App\Models\Admin;
 use Illuminate\Support\Facades\Hash;
 
 class AuthService
 {
-   protected Admin $admin;
-   public function __construct(Admin $admin)
-   {
-       $this->admin = $admin;
-   }
+    protected Admin $admin;
 
-  public function login(array $credentials): bool
-{
-    if (Auth::guard('employee')->attempt($credentials)) {
-
-        $employee = Auth::guard('employee')->user();
-
-        if (!$employee->is_approved) {
-
-            Auth::guard('employee')->logout();
-
-            return false;
-        }
-
-        return true;
+    public function __construct(Admin $admin)
+    {
+        $this->admin = $admin;
     }
 
-    return false;
-}
+    public function login(
+        array $credentials
+    ): bool {
 
-   public function register(array $data) : Admin {
-    return $this->admin->create([
-        'name' => $data['name'],
-        'email'=> $data['email'],
-        'password' => Hash::make($data['password']),
-    ]);
-   }
+        return Auth::guard('admin')
+            ->attempt($credentials);
+    }
 
-   public function logout(): void {
-    Auth::guard('admin')->logout();
-   }
+    public function register(
+        array $data
+    ): Admin {
+
+        return $this->admin->create([
+
+            'name' => $data['name'],
+
+            'email'=> $data['email'],
+
+            'password' => Hash::make(
+                $data['password']
+            ),
+        ]);
+    }
+
+    public function logout(): void
+    {
+        Auth::guard('admin')->logout();
+    }
 }
