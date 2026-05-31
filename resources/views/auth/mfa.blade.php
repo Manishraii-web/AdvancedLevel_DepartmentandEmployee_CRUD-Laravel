2d1a9fca868a5d07571c221a938df97184cedb8c
Manishraii-web <raimanish489@gmail.com>
2026-05-31 14:42:53 +0545

    mchanges

---
+++ resources/views/auth/mfa.blade.php
@@ -0,0 +1,13 @@
+<form method="POST" action="{{ route('admin.mfa.verify') }}">
+    @csrf
+
+    <h2>Enter OTP</h2>
+
+    <input type="text" name="otp" placeholder="6-digit OTP">
+
+    @error('otp')
+        <p style="color:red">{{ $message }}</p>
+    @enderror
+
+    <button type="submit">Verify</button>
+</form>
